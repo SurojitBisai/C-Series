c68256b53841b938f128b1d70c2b130eb1f9f3fe
surojit Bisai <surojitbisai8328@gmail.com>
2025-04-19 15:33:12 +0530

    Ternary Statement

---
+++ Ternary_Statement.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n ;
+    cout << "enter number : ";
+    cin >> n;
+    cout << (n >= 0 ? "positive" : "negative") << endl;
+    return 0;
+}
+
+
+ 
