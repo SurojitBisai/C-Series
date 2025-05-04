a005b43fb2b25c704bc637c89f4717e25b511712
surojit Bisai <surojitbisai8328@gmail.com>
2025-05-04 23:17:31 +0530

    Function in Memory

---
+++ function_in_memory.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int changex(int x){
+    x = 2*x;
+    cout << "x = " << x << endl;
+    
+}
+
+int main(){
+
+  int x = 5;
+  changex(x);
+  cout << "x = " << x << endl;
+
+    return 0;
+
+}
+
+    
